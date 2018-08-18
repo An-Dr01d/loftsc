a96799cf0d19135b36bf44bde9f805e168aabc1d
Android <androidworking@mail.ru>
2018-08-18 13:56:08 +0300

    Задание 1

---
+++ test/Homework.php
@@ -0,0 +1,10 @@
+<?php
+echo "Задание #1";
+echo "<br/>";
+$name = 'Игорь';
+$age = 29;
+echo "<br/>Меня зовут: $name";
+echo "<br/>Мне $age лет";
+echo "<br/> “!|\/’”\ ";
+echo "<br> Задание #2";
+echo "<br/>";
