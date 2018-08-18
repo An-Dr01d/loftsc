2d3b1bb3d33e8d48e4873dd6e6e5a48446f996f3
Android <androidworking@mail.ru>
2018-08-18 15:33:10 +0300

    Задание 2

--- homework.php
+++ homework.php
@@ -1,10 +1,15 @@
 <?php
 echo "Задание # 1";
-echo "<br/>";
+
 $name = 'Игорь';
 $age = 29;
 echo "<br/>Меня зовут: $name";
 echo "<br/>Мне $age лет";
 echo "<br/> “!|\/’”\ ";
+echo "<br/>";
 echo "<br> Задание # 2";
 echo "<br/>";
+const ALL_IMAGINE = '80'; // всего рисунков
+const MARKER_IMAGINE = '23'; // рисунки фломастерами
+$paint_imagine = ALL_IMAGINE - MARKER_IMAGINE;
+echo "На школьной выставке было $paint_imagine рисунков красками.";
