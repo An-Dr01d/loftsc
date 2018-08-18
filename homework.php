90f44ea4107608d657076189057aeb5831acdf4b
Android <androidworking@mail.ru>
2018-08-18 17:43:12 +0300

    Задание 4

--- homework.php
+++ homework.php
@@ -31,3 +31,25 @@ if($age>=18 && $age<=65) {
 {
     echo "Неизвестный возраст";
 }
+echo "<br/>";
+echo "<br> Задание # 4";
+echo "<br/>";
+$day = 10;
+switch ($day)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    echo "Это рабочий день";
+        break;
+    case 6:
+    case 7:
+    echo "Это выходной день";
+    break;
+    default:
+    Echo "Неизвестный день";
+    break;
+    
+}
