f59e79e208997bb2101b0c78dc9b321cba0e1609
Android <androidworking@mail.ru>
2018-08-18 17:01:49 +0300

    Задание 3

--- homework.php
+++ homework.php
@@ -8,8 +8,26 @@ echo "<br/>Мне $age лет";
 echo "<br/> “!|\/’”\ ";
 echo "<br/>";
 echo "<br> Задание # 2";
-echo "<br/>";
 const ALL_IMAGINE = '80'; // всего рисунков
 const MARKER_IMAGINE = '23'; // рисунки фломастерами
 $paint_imagine = ALL_IMAGINE - MARKER_IMAGINE;
 echo "На школьной выставке было $paint_imagine рисунков красками.";
+echo "<br/>";
+echo "<br> Задание # 3";
+echo "<br/>";
+$age = 66;
+if($age>=18 && $age<=65) {
+    echo 'Вам еще работать и работать';
+}
+    elseif ($age>65)
+{
+    echo "Вам пора на пенсию";
+}
+    elseif ($age>=1 && $age<=17)
+{
+    echo "Вам еще рано работать";
+}
+    else
+{
+    echo "Неизвестный возраст";
+}
