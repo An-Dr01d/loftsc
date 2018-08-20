2ed97b3304d8ae64d17dde92150827828c896644
Android <androidworking@mail.ru>
2018-08-20 16:36:50 +0300

    Форматирование  кода

--- homework.php
+++ homework.php
@@ -17,47 +17,39 @@ echo "Рисунки красками = Всего рисунков - Рисун
 const ALL_IMAGINE = '80'; // всего рисунков
 const MARKER_IMAGINE = '23'; // рисунки фломастерами
 const PEN_IMAGINE = '40'; // рисунки карандашами
-echo "<b>Ответ:</b> На школьной выставке было ", ALL_IMAGINE - MARKER_IMAGINE - PEN_IMAGINE, " рисунков красками."; // рисунки красками
+echo "На выставке было ", ALL_IMAGINE - MARKER_IMAGINE - PEN_IMAGINE, " рисунков красками."; // рисунки красками
 echo "<br/>";
 echo "<br> Задание # 3";
 echo "<br/>";
 $age = 66;
-if($age>=18 && $age<=65) {
+if ($age>=18 && $age<=65) {
     echo 'Вам еще работать и работать';
-}
-    elseif ($age>65)
-{
+} elseif ($age>65) {
     echo "Вам пора на пенсию";
-}
-    elseif ($age>=1 && $age<=17)
-{
+} elseif ($age>=1 && $age<=17) {
     echo "Вам еще рано работать";
-}
-    else
-{
+} else {
     echo "Неизвестный возраст";
 }
 echo "<br/>";
 echo "<br> Задание # 4";
 echo "<br/>";
 $day = 10;
-switch ($day)
-{
+switch ($day) {
     case 1:
     case 2:
     case 3:
     case 4:
     case 5:
-    echo "Это рабочий день";
+           echo "Это рабочий день";
         break;
     case 6:
     case 7:
-    echo "Это выходной день";
-    break;
+            echo "Это выходной день";
+        break;
     default:
-    Echo "Неизвестный день";
-    break;
-    
+            echo "Неизвестный день";
+        break;
 }
 echo "<br/>";
 echo "<br> Задание # 5";
@@ -86,18 +78,18 @@ $rows = 10;
 $cols = 10;
 
 $table = '<table border="1">';
-
-for ($tr=1; $tr<=$rows; $tr++){
+for ($tr=1; $tr<=$rows; $tr++) {
     $table .= '<tr>';
-    for ($td=1; $td<=$cols; $td++){
-        if ($tr%2==0 && $td%2==0){
+    for ($td=1; $td<=$cols; $td++) {
+        if ($tr%2==0 && $td%2==0) {
             $table .= '<td>('. $tr*$td .')</td>'; // вычислили первую строку или столбец
-        }elseif ($tr%2!=0 && $td%2!=0){
+        } elseif ($tr%2!=0 && $td%2!=0) {
             $table .= '<td>['. $tr*$td .']</td>'; // все ячейки, кроме ячеек из первого столбца и первой строки
-        }else{
+        } else {
             $table .= '<td>'. $tr*$td .'</td>';
         }
     }
+
     $table .= '</tr>';
 }
 
