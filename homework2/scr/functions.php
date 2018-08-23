a5020dc6da9f4ce8d792f076a655a2fe47a94fe7
Android <androidworking@mail.ru>
2018-08-23 10:51:10 +0300

    Задание 1

--- homework2/scr/functions.php
+++ homework2/scr/functions.php
@@ -1,4 +1,6 @@
 <?php
+echo "<br/>Функция 1";
+echo "<br/>";
 $array = array('имя', 'почта', 'телефон');
 function task1($array, $second = true)
 {
@@ -11,3 +13,62 @@ function task1($array, $second = true)
     }
 }
 echo task1($array, $second = true);
+
+echo "<br/>Функция 2";
+echo "<br/>";
+function task2(...$args) // Добавляем для первого аргумента переменную
+{
+    $operator = array_shift($args);
+    echo implode($args, $operator) . ' = ';
+    $score = 0; // Переменная, которая помогает избежать проблем при вычислении
+    foreach ($args as $key => $v) {
+        if ($score > 1) { // Условия, для исключения вычислений с первым символом
+            switch ($operator) {
+                case '-':
+                    $score -= $v;
+                    break;
+                case '+':
+                    $score += $v;
+                    break;
+                case '*':
+                    $score *= $v;
+                    break;
+                case '/':
+                    $score /= $v;
+                    break;
+            }
+        } else {
+            $score = $v;
+        }
+    }
+    echo $score;
+}
+echo task2('-', 2, 2, 10, 5.4);
+
+echo "<br/>Функция 3";
+echo "<br/>";
+function task3($rows, $cols)
+{
+    if ($rows < 1 || $cols < 1) {
+        echo "Ошибка";
+    } else {
+        echo $table = '<table border="1">';
+        for ($tr = 1; $tr <= $rows; $tr++) {
+            $table .= '<tr>';
+            for ($td = 1; $td <= $cols; $td++) {
+                $table .= '<td>' . $tr * $td . '</td>';
+            }
+        }
+
+        $table .= '</tr>';
+
+        $table .= '</table>';
+        echo $table;
+    }
+}
+task3(-8, 8);
+
+echo "<br/>";
+
+
+
